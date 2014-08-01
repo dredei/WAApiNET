55113be656fae3cda7be50fd930e80df566e795b
dredei <xkadidas@gmail.com>
2014-08-01 18:45:23 +0300

    Пароль для тестов вынесен в отдельный файл

--- C#/WAApiNETTests/AccountTests.cs
+++ C#/WAApiNETTests/AccountTests.cs
@@ -1,5 +1,6 @@
 ﻿#region Using
 
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WAApiNET;
@@ -11,10 +12,21 @@ namespace WAApiNETTests
     [TestClass]
     public class AccountTests
     {
+        private readonly string _email;
+        private readonly string _password;
+
+        public AccountTests()
+        {
+            var content = File.ReadAllText( "loginInfo.txt" );
+            string[] arr = content.Split( ';' );
+            this._email = arr[ 0 ];
+            this._password = arr[ 1 ];
+        }
+
         [TestMethod]
         public async Task TestGeneralInfo()
         {
-            var waapi = new WAApi( "softezz_team@list.ru", "UuBC9zI0S0RNZH-k" );
+            var waapi = new WAApi( this._email, this._password );
             await waapi.Account.SignIn();
             await waapi.Account.GetGeneralInfo();
             string login = waapi.Account.CurrentAccount.Login;
