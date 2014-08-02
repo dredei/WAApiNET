4268ca0a829e79b79b8df32d15cbbbd58c3483a1
dredei <xkadidas@gmail.com>
2014-08-02 14:34:48 +0300

    Добавлены тесты для добавление, переименования и удаления папки

--- C#/WAApiNETTests/FolderTests.cs
+++ C#/WAApiNETTests/FolderTests.cs
@@ -1,6 +1,7 @@
 ﻿#region Using
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WAApiNET;
@@ -31,6 +32,21 @@ namespace WAApiNETTests
             Assert.IsTrue( folders.Count >= 3 && folders[ 0 ].Name == "12" && folders[ 1 ].Name == "3" &&
                            folders[ 2 ].Name == "test" && folders[ 0 ].Tasks >= 5 &&
                            folders[ 1 ].Tasks >= 1 && folders[ 2 ].Tasks >= 0 );
+        }
+
+        [TestMethod]
+        public async Task TestAddChangeDeleteFolder()
+        {
+            await this._waApi.Account.SignIn();
+            Folder newFolder = await this._waApi.Folder.AddFolder( "FolderFromTests" );
+            List<Folder> folders = await this._waApi.Folder.GetFolders();
+            int foldersCountAfterAdd = folders.Count;
+            await this._waApi.Folder.SetFolderName( newFolder, "FolderFromTests2" );
+            folders = await this._waApi.Folder.GetFolders();
+            bool newNameFolderFound = folders.SingleOrDefault( f => f.Name == "FolderFromTests2" ) != null;
+            await this._waApi.Folder.DeleteFolders( new[] { newFolder } );
+            folders = await this._waApi.Folder.GetFolders();
+            Assert.IsTrue( foldersCountAfterAdd == 4 && newNameFolderFound && folders.Count == 3 );
         }
     }
 }
