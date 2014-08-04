98d03d29fdee3582bd6c9045b1862468599c7654
dredei <xkadidas@gmail.com>
2014-08-04 19:28:27 +0300

    Реализовано клонирование задания (closed #5)

---
+++ C#/WAApiNET/ServerQueries/CloneTaskQuery.cs
@@ -0,0 +1,39 @@
+﻿#region Using
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace WAApiNET.ServerQueries
+{
+    internal class CloneTaskQuery : BaseQueryData
+    {
+        [JsonProperty( PropertyName = "Folder ID" )]
+        public int? FolderId { get; set; }
+
+        [JsonProperty( PropertyName = "Target ID" )]
+        public int? TargetId { get; set; }
+
+        [JsonProperty( PropertyName = "Task ID" )]
+        public int? TaskId { get; set; }
+
+        public string Name { get; set; }
+
+        public CloneTaskQuery()
+        {
+            this.FolderId = null;
+            this.TargetId = null;
+            this.TaskId = null;
+            this.Name = null;
+        }
+
+        public CloneTaskQuery( string token, int? folderId, int? targetId, int? taskId, string name )
+            : base( token )
+        {
+            this.FolderId = folderId;
+            this.TargetId = targetId;
+            this.TaskId = taskId;
+            this.Name = name;
+        }
+    }
+}
