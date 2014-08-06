c7a2ca257a0e271568e1daa2f6e5a2f685a80082
dredei <xkadidas@gmail.com>
2014-08-06 17:19:10 +0300

    Добавлено сравнение для класса WAFolderWhole

--- C#/WAApiNET/Model/Folder/WAFolderWhole.cs
+++ C#/WAApiNET/Model/Folder/WAFolderWhole.cs
@@ -1,5 +1,6 @@
 ﻿#region Using
 
+using System;
 using System.Collections.Generic;
 using WAApiNET.Model.Task;
 
@@ -10,7 +11,7 @@ namespace WAApiNET.Model.Folder
     /// <summary>
     /// Объект папки (со списком заданий)
     /// </summary>
-    public class WAFolderWhole : WAFolder
+    public class WAFolderWhole : WAFolder, IEquatable<WAFolderWhole>
     {
         /// <summary>
         /// Список заданий
@@ -24,5 +25,57 @@ namespace WAApiNET.Model.Folder
         {
             this.Tasks = new List<WATaskWhole>();
         }
+
+        #region Members
+
+        public bool Equals( WAFolderWhole other )
+        {
+            if ( ReferenceEquals( null, other ) )
+            {
+                return false;
+            }
+            if ( ReferenceEquals( this, other ) )
+            {
+                return true;
+            }
+            return base.Equals( other ) && Equals( this.Tasks, other.Tasks );
+        }
+
+        public override bool Equals( object obj )
+        {
+            if ( ReferenceEquals( null, obj ) )
+            {
+                return false;
+            }
+            if ( ReferenceEquals( this, obj ) )
+            {
+                return true;
+            }
+            if ( obj.GetType() != this.GetType() )
+            {
+                return false;
+            }
+            return this.Equals( (WAFolderWhole)obj );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ( base.GetHashCode() * 397 ) ^ ( this.Tasks != null ? this.Tasks.GetHashCode() : 0 );
+            }
+        }
+
+        public static bool operator ==( WAFolderWhole left, WAFolderWhole right )
+        {
+            return Equals( left, right );
+        }
+
+        public static bool operator !=( WAFolderWhole left, WAFolderWhole right )
+        {
+            return !Equals( left, right );
+        }
+
+        #endregion
     }
 }
