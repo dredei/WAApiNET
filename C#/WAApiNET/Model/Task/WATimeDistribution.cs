d70f47419f294b1df91b203b36798d3107470cf5
dredei <xkadidas@gmail.com>
2014-08-06 17:29:36 +0300

    Добавлено сравнение для класса WATimeDistribution

--- C#/WAApiNET/Model/Task/WATimeDistribution.cs
+++ C#/WAApiNET/Model/Task/WATimeDistribution.cs
@@ -1,9 +1,15 @@
-﻿namespace WAApiNET.Model.Task
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace WAApiNET.Model.Task
 {
     /// <summary>
     /// Объект временного распределения
     /// </summary>
-    public class WATimeDistribution
+    public class WATimeDistribution : IEquatable<WATimeDistribution>
     {
         /// <summary>
         /// Процент
@@ -34,5 +40,57 @@
             this.Percent = percent;
             this.Priority = priority;
         }
+
+        #region Members
+
+        public bool Equals( WATimeDistribution other )
+        {
+            if ( ReferenceEquals( null, other ) )
+            {
+                return false;
+            }
+            if ( ReferenceEquals( this, other ) )
+            {
+                return true;
+            }
+            return this.Percent == other.Percent && this.Priority == other.Priority;
+        }
+
+        public override bool Equals( object obj )
+        {
+            if ( ReferenceEquals( null, obj ) )
+            {
+                return false;
+            }
+            if ( ReferenceEquals( this, obj ) )
+            {
+                return true;
+            }
+            if ( obj.GetType() != this.GetType() )
+            {
+                return false;
+            }
+            return this.Equals( (WATimeDistribution)obj );
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ( this.Percent.GetHashCode() * 397 ) ^ this.Priority.GetHashCode();
+            }
+        }
+
+        public static bool operator ==( WATimeDistribution left, WATimeDistribution right )
+        {
+            return Equals( left, right );
+        }
+
+        public static bool operator !=( WATimeDistribution left, WATimeDistribution right )
+        {
+            return !Equals( left, right );
+        }
+
+        #endregion
     }
 }
